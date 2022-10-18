44e901489e80e28775a0803c64d23ffbe9e50047
SuperiorAlpha <>
2022-10-18 21:43:38 +0800

    m-1018-02

--- K3_demo.py
+++ K3_demo.py
@@ -2,7 +2,5 @@ from k3cloud_webapi_sdk.util import encode_util
 
 r = encode_util.DecodeAppSecret('fb7d46647ac346dbb5fcd45b65512a45')
 
-l = len('fb7d46647ac346dbb5fcd45b65512a45')
 
 print(r)
-print(l)
